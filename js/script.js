3b08b1bfd1ad10093bee5f27b1d15313b0a4a565
Muhammad AlMutaz <muhalmutaz@ksu.edu.sa>
2024-04-26 03:36:41 +0300

    new style

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":980},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
